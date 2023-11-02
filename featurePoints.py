8756e411e2bc8b760d81e52a497dd7ff1a9687ba
sivuu96 <jsivsankar@gmail.com>
2023-11-02 20:07:20 +0530

    feature point detection

---
+++ featurePoints.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KDTree
+
+lidar_data = pd.read_csv('station7_market.csv')
+
+# Define a function to calculate the normal vectors for each point
+def calculate_normals(points, k=100):
+    tree = KDTree(points)
+    normals = []
+
+    for point in points:
+        _, indices = tree.query([point], k=k)
+        neighbors = points[indices[0]]
+        
+        # Calculate the covariance matrix
+        covariance_matrix = np.cov(neighbors, rowvar=False)
+
+        # Compute the eigenvalues and eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+
+        # Get the normal vector associated with the smallest eigenvalue
+        normal = eigenvectors[:, np.argmin(eigenvalues)]
+
+        normals.append(normal)
+
+    return np.array(normals)
+
+lidar_points = lidar_data[['x', 'y', 'z']].values
+lidar_normals = calculate_normals(lidar_points)
+
+threshold = 0.000001
+
+feature_indices = []
+dot_products = []
+num_feature_normals=0
+
+# Iterate through the LiDAR points and check for feature points
+for i, point in enumerate(lidar_points):
+    neighbor_normals = lidar_normals[i]
+
+    # Calculate the dot product of the normal vectors
+    dot_products = np.dot(neighbor_normals, lidar_normals.T)
+    # Count the number of normals with a dot product below the threshold
+    num_feature_normals = np.sum(dot_products < threshold)
+
+print(num_feature_normals)
+j=0
+while(j<len(dot_products)):
+    if(dot_products[j]<threshold):
+        feature_indices.append(j)
+    j=j+1
+
+feature_points = lidar_points[feature_indices]
+
+pd.DataFrame(feature_points, columns=['x', 'y', 'z']).to_csv('station7_market_0.000001-thr.csv', index=False)
